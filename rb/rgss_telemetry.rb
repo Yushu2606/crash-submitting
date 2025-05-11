57d0dea9c493d6ab5071d055f58151be322dfd0b
予纾 <yushu@srcz.one>
2025-05-11 13:06:02 +0800

    fix: url will not be released

--- rb/rgss_telemetry.rb
+++ rb/rgss_telemetry.rb
@@ -1,18 +1,16 @@
-VERSION = "1.4"
+VERSION = "1.5"
+START_TELEMETRY = Win32API.new("rgss_telemetry", "start_hook", "pp", "i")
 
 def main
-  return unless File.exist?("./rgss_telemetry.dll")
-
   getPrivateProfileString = Win32API.new("kernel32", "GetPrivateProfileString", "ppppip", "i")
   buffer = [].pack("x256")
   l = getPrivateProfileString.call("Telemetry", "Url", "", buffer, buffer.size, "./Game.ini")
   url = buffer[0, l]
   return if url.nil? or url.empty?
 
-  start_hook = Win32API.new("rgss_telemetry", "start_hook", "pp", "i")
-  hook_result = start_hook.call(url, VERSION)
-  raise unless hook_result == 0
-  return start_hook
+  start_result = START_TELEMETRY.call(url, VERSION)
+  raise "Telemetry startup failed." unless start_result == 0
+  return url
 end
 
-START_HOOK = main
+URL = main
